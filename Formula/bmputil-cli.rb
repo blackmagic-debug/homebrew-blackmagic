fa92b0b500052d943021a720f9f4a9ab83310d86
Piotr Esden-Tempski <piotr@esden.net>
2025-07-21 03:44:13 +0000

    bmputil-cli: add 1.0.0 bottle.

--- Formula/bmputil-cli.rb
+++ Formula/bmputil-cli.rb
@@ -11,6 +11,13 @@ class BmputilCli < Formula
     strategy :github_latest
   end
 
+  bottle do
+    root_url "https://github.com/blackmagic-debug/homebrew-blackmagic/releases/download/bmputil-cli-1.0.0"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "a067bdf80e2767000b7baad3bc36b0cfd1edd2c08625590a134892af20c3fae1"
+    sha256 cellar: :any_skip_relocation, ventura:       "4eaa480bb4d4135ebe1d63176c6b021ba9066a79be780fcf4db5e6aea811bc32"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "33aca5ca4454e242b3597e1280c7e48c0a9c2d5120f7fee61885b4ba7d6af8d3"
+  end
+
   depends_on "rust" => :build
   depends_on "openssl"
 
